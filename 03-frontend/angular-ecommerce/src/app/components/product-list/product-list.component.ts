4deaac8e2fc098fb32d5a54117a39040040d856c
milossesic <milosh.sesic@gmail.com>
2020-04-18 22:02:50 +0200

    added category in product list grid

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products:Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
   constructor(private productService:ProductService,
               private route:ActivatedRoute) { }
 
@@ -26,13 +27,17 @@ export class ProductListComponent implements OnInit {
 //check if id parameter is available
 const hasCategoryId:boolean = this.route.snapshot.paramMap.has('id');
 
-    if(hasCategoryId){
-      //get the id param string. convert string to a number with th + magic
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-    } else {
-      //if nothing give it 1
-      this.currentCategoryId = 1;
-    }
+if (hasCategoryId) {
+  // get the "id" param string. convert string to a number using the "+" symbol
+  this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+  // get the "name" param string
+  this.currentCategoryName = this.route.snapshot.paramMap.get('name');
+} else {
+  // not category id available ... default to category id 1
+  this.currentCategoryId = 1;
+  this.currentCategoryName = 'Books';
+}
 
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
